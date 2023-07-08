57acc9ba67e9266cc5e2e2d2b3b77bf8249339cd
Tristan Hanson-Suggitt <tristan@wataq.co>
2023-07-08 10:51:05 -0700

    added config template

---
+++ config_template.py
@@ -0,0 +1,30 @@
+
+class C:
+	def __init__(self):
+		self.TOKEN = ''
+
+		self.BESTOWMENT_CHANNEL = 0
+		self.LOBBY_CHANNEL = 0
+		self.LOG_CHANNEL = 0
+		self.PRIVATE_LOG_CHANNEL = 0
+		self.PUBLIC_LOG_CHANNEL = 0
+		self.SPAM_CHANNELS = [
+			0,
+			0
+		]
+		self.SEX_GIFS_CHANNEL = 0
+
+		self.ACTIVE_ROLE = 0
+		self.BESTOWER_ROLE = 0
+		self.HE_ROLE = 0
+		self.SHE_ROLE = 0
+		self.THEY_ROLE = 0
+
+		self.GUILD = 0
+
+		self.INVITE_DURATION = 0
+
+		self.TAQ = 0
+		self.EG = 0
+
+config = C()
